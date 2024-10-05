43e2f35d91dab6e5122631fa2d8848229d78f42e
Chris Zavala <zavastopher@gmail.com>
2024-10-05 15:14:10 -0400

    Started adding a markdown previewer

--- init.lua
+++ init.lua
@@ -704,15 +704,6 @@ require('lazy').setup({
     end,
   },
 
-  {
-    'iamcco/markdown-preview.nvim',
-    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
-    ft = { 'markdown' },
-    build = function()
-      vim.fn['mkdp#util#install']()
-    end,
-  },
-
   -- Highlight todo, notes, etc in comments
   {
     'folke/todo-comments.nvim',
