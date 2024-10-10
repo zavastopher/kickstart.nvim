341de0c96ff751699863ee85cdc0c1a1a232fe5c
Chris Zavala <zavastopher@gmail.com>
2024-10-10 16:41:09 -0400

    Fixed tsserver to ts_ls name change

--- init.lua
+++ init.lua
@@ -498,7 +498,7 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`tsserver`) will work just fine
-        tsserver = {},
+        ts_ls = {},
         --
         --html-lsp = {},
         lua_ls = {
